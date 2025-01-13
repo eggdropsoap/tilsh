b12af7df27bc1af27380cadd1db3a7763c9d0542
eggdropsoap <eggdropsoap@pipemaze.com>
2025-01-12 22:19:05 -0800

    Partial implementation of digestBody()

--- src/tlsh.js
+++ src/tlsh.js
@@ -1,10 +1,17 @@
 import pearson from './pearson.js';
 
-let testSize;
+let testMinSize;
 // testSize = 20;
-const defaultSize = 50;         // new style, but requires a minimum amount of randomness
-const conservativeSize = 256;   // old style, for compatibility
-let minSize = testSize || conservativeSize;
+const defaultMinInputSize = 50;         // new style, but requires a minimum amount of randomness
+const conservativeMinInputSize = 256;   // old style, for compatibility
+const digestBodyLength = {  // in bytes, not hex
+    original: 32,   // standard
+    expanded: 64,   // now possible with 256 buckets
+}
+const digestByteLength = digestBodyLength.original; 
+const bucketArraySize = digestByteLength * 4;
+
+let minSize = testMinSize || conservativeMinInputSize;
 
 const windowSize = 5;
 
@@ -17,7 +24,7 @@ export default function hash (s) {
     console.log(`u8array\n[ ${u8array} ]\n`);
 
     // 1. Process the byte string `u8array` with sliding window 5-wide into Pearson buckets
-    const buckets = new Uint8Array(256);
+    const buckets = new Uint8Array(bucketArraySize);
     console.log(`buckets before\n[ ${buckets} ]\n`);
     for (let windowEnd = windowSize; windowEnd <= u8array.length; windowEnd++) {
         const windowStart = windowEnd - windowSize;
@@ -33,35 +40,63 @@ export default function hash (s) {
     console.log(`buckets after\n[ ${buckets} ]\n`);
 
     // 2. calcualte the quartiles
-    const [q1,q2,q3] = quartiles(buckets);
+    let q1, q2, q3;
+    [q1,q2,q3] = quartiles(buckets);
     console.log("q1 %d, q2 %d, q3 %d",q1,q2,q3)
     console.log(buckets);
 
     // 3. construct the digest header
-
+    let header = versionMark;   // stub
 
     // 4. construct the digest body
+    const body = digestBody(buckets,q1,q2,q3);
+    console.log("RESULT: %s", body);
 
 
-    return result = null;
+    return `${header}${body}`;
+}
+
+function digestBody (buckets,q1,q2,q3) {
+    console.log("digestBody args: ", arguments);
+    const len = buckets.length;
+    let accumulator = new Uint8ClampedArray(digestByteLength);
+    for (let bi = 0; bi < len; bi+=4) {
+        let byte = 0;
+        for (let j = 0; j < 4; j++) {
+            let emit = null;
+            if      ( buckets[bi+j] <= q1 ) emit = 0;
+            else if ( buckets[bi+j] <= q2 ) emit = 1;
+            else if ( buckets[bi+j] <= q3 ) emit = 2;
+            else                            emit = 3;
+    
+            if ( emit === null ) throw new Error("digest body construction failed to generate bits");
+            byte = (byte << 2) + emit;
+            console.log(`    emit: ${emit}, byte_acc: ${byte}, binary: ${byte.toString(2).padStart(8,'0')}`)
+        }
+        let index = Math.floor(bi/4);
+        console.log(`Iternation output index: ${index}`)
+        accumulator[index] = (byte);
+        console.log(`loop ${bi}: byte is ${byte}, accumulator is ${accumulator} (length ${accumulator.length})`);
+    }
+    console.log(`final accumulator: ${accumulator.length}`)
+    let hex = [];
+    hex = accumulator.map((x) => x.toString(16));
+    // function ToHex(a) {
+    //     for
+    // }
+    // const result = ToHex(accumulator);
+    console.log("type of accumulator: ",typeof(hex))
+    return hex;
 }
 
 function selectTriplet(i,arr){
-    const pattern = `
-        1. A B C
-        2. A B   D
-        3. A B     E
-        4. A   C D
-        5. A   C   E
-        6. A     D E
-    `
     const mask = [
-        [0,1,2],
-        [0,1,3],
-        [0,1,4],
-        [0,2,3],
-        [0,2,4],
-        [0,3,4],
+        [0,1,2],    // 1. A B C
+        [0,1,3],    // 2. A B   D
+        [0,1,4],    // 3. A B     E
+        [0,2,3],    // 4. A   C D
+        [0,2,4],    // 5. A   C   E
+        [0,3,4],    // 6. A     D E
     ]
     function select(pattern,pos) {
         return mask[pattern][pos];
